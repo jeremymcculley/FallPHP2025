24582ef19abc9bfbad23ae0953da259b01f7e0ec
jeremymcculley <50459396+jeremymcculley@users.noreply.github.com>
2025-10-28 10:00:26 -0400

    Add files via upload
    Updated the database file to contain our connection class

--- create-read-update-with-images-lesson/includes/database.php
+++ create-read-update-with-images-lesson/includes/database.php
@@ -1,2 +1,18 @@
 <?php
-
+class Database{
+    private $host = "localhost";
+    private $db_name = "phpclass";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+    public function connect(){
+        $this->conn = null;
+        try{
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(PDOExecption $e){
+            echo "<p>Connection Error: " . $e->getMessage()"</p>";
+        }
+        return $this->conn;
+    }
+}
