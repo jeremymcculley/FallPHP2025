602ceb942cb6e2a2afe3e81ca82c75f31fad6313
jeremymcculley <50459396+jeremymcculley@users.noreply.github.com>
2025-10-28 10:17:17 -0400

    Add files via upload
    created a create function, get all function, and a get by id function. Still need to create the update function

---
+++ create-read-update-with-images-lesson/includes/user.php
@@ -0,0 +1,30 @@
+<?php
+class User{
+    private $conn;
+    private $table = 'userRecords';
+    public function __construct($db){
+        $this->conn = $db;
+    }
+    // our create function
+    public function create($name, $email, $image){
+        $sql = "INSERT INTO {$this->table} (name, email, image) VALUES (:name, :email, :image)";
+        $stmt = $this->conn->prepare($sql);
+        return $stmt->execute([
+            ':name' => $name,
+            ':email' => $email,
+            ':image' => $image
+        ]);
+    }
+    // get all the data from our table
+    public function getAll(){
+        $sql = "SELECT * FROM {$this->table} ORDER BY id ASC";
+        $stmt = $this->conn->query($sql);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    // get a record by id
+    public function getById($id){
+        $stmt = $this->conn->prepare("SELECT * FROM {$this->table} WHERE id = ?");
+        $stmt->execute->([$id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
